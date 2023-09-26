42dc3472ff5a9dcfad6c2ebcd683bb05b02981d7
Ian Jeremy Guani <ianguani@yahoo.ca>
2023-09-26 12:13:27 -0700

    bug fix for lab 3

--- COMP4537/Labs/3/server.js
+++ COMP4537/Labs/3/server.js
@@ -9,13 +9,12 @@ const port = 3000
 http.createServer((req, res) => {
     let q = url.parse(req.url, true)
     res.writeHead(200, {'Content-Type': 'text', 'Access-Control-Allow-Origin': '*'})
-    if (q.pathname == "COMP4537/labs/3/getDate") {
+    if (q.pathname == "COMP4537/Labs/3/getDate") {
         res.write(`<p style="color: blue;">Hello ${q.query["name"]}. What a beautiful day. Server current date and time is ${dt.date()}</p>`)
-        res.end()
     } else {
         res.write("404")
-        res.end()
     }    
+    res.end()
 }).listen(port)
 
 console.log("Server is running and listening on port: " + port)
