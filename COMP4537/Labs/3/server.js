f4a3448360bd139cab0a222b0263c5612770bd86
inaguu <ianguani@yahoo.ca>
2023-09-26 21:10:04 -0700

    bonus marks for lab 3 (need help)

--- COMP4537/Labs/3/server.js
+++ COMP4537/Labs/3/server.js
@@ -7,15 +7,44 @@ const dt = require('./modules/utils')
 const port = 3000
 
 http.createServer((req, res) => {
-    let q = url.parse(req.url, true)
-    console.log(q.pathname)
-    res.writeHead(200, {'Content-Type': 'text', 'Access-Control-Allow-Origin': '*'})
+    const q = url.parse(req.url, true)
+    
+    if (q.pathname == "/COMP4537/Labs/3/writeFile/") {
+        fs.appendFile("file.txt", q.query["text"], (err) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                return res.end()
+            } 
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+            res.write(`<p style="color: blue;">File writen successfully</p>`)
+            console.log(`File write ${q.query["text"]}`)
+            return res.end()
+        })
+    } 
+
+    if (q.pathname == "/COMP4537/Labs/3/readFile/") {
+        fs.readFile("file.txt", "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                return res.end()
+            } 
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+            res.write(`<p style="color: blue;">${data}</p>`)
+            console.log(data)
+            return res.end()
+        })
+    } 
+
     if (q.pathname == "/COMP4537/Labs/3/getDate/") {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
         res.write(`<p style="color: blue;">Hello ${q.query["name"]}. What a beautiful day. Server current date and time is ${dt.date()}</p>`)
-    } else {
-        res.write("404")
-    }    
+        return res.end()
+    }  
+
+    res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+    res.write(`404`)
     res.end()
+
 }).listen(port)
 
 console.log("Server is running and listening on port: " + port)
