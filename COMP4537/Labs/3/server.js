19da95f1596e229ba24ce51ec42c230189412d4e
inaguu <ianguani@yahoo.ca>
2023-10-01 19:17:04 -0700

    school :)

--- COMP4537/Labs/3/server.js
+++ COMP4537/Labs/3/server.js
@@ -21,13 +21,13 @@ http.createServer((req, res) => {
             console.log(`File write ${q.query["text"]}`)
             return res.end()
         })
-    } 
-
-    if (q.pathname == "/COMP4537/Labs/3/readFile/") {
-        fs.readFile("file.txt", "utf8", (err, data) => {
+    } else if (q.pathname.includes("readFile")) {
+        let file = q.pathname.substring(q.pathname.lastIndexOf('/') + 1)
+        fs.readFile(file, "utf8", (err, data) => {
             if (err) {
                 console.log(err)
                 res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                res.write(`404 - no such file or directory <br> ${q.pathname.substring(q.pathname.lastIndexOf('/') + 1)}`)
                 return res.end()
             } 
             res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
@@ -35,13 +35,15 @@ http.createServer((req, res) => {
             console.log(data)
             return res.end()
         })
-    } 
-
-    if (q.pathname == "/COMP4537/Labs/3/getDate/") {
+    } else if (q.pathname == "/COMP4537/Labs/3/getDate/") {
         res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
         res.write(`<p style="color: blue;">Hello ${q.query["name"]}. What a beautiful day. Server current date and time is ${dt.date()}</p>`)
         return res.end()
-    }  
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+        res.write(`<p style="color: blue;">home page</p>`)
+        return res.end()
+    }
 
 }).listen(port)
 
